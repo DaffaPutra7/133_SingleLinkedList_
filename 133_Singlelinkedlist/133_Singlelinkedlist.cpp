5183bb8a02db4af6d4076ad32937d232af7611c4
DaffaPutra7 <putradika3604@gmail.com>
2023-05-18 22:46:51 +0700

    Int main [ Int nim, Case 3, Case 4]

--- 133_Singlelinkedlist/133_Singlelinkedlist.cpp
+++ 133_Singlelinkedlist/133_Singlelinkedlist.cpp
@@ -153,6 +153,32 @@ int main() {
 				system("pause");
 				system("cls");
 				break;
+			case 2 :
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "Masukkan NIM : ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim : " << nim << "berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
+			case 3 :
+				traverse();
+				break;
+			case 4 : 
+				searchData();
+				break;
 
 			}
 		}
